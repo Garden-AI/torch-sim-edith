d0d831f85c29ddafd56e97494d9bb21fc39d85fd
hholb <hholbrook@uchicago.edu>
2025-08-01 10:35:21 -0400

    clean up, add env files

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM python:3.12
-ARG ENDPOINT_VERSION
-RUN pip install globus-compute-endpoint==${ENDPOINT_VERSION}
--- ssf_job.sh
+++ ssf_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -N torch-sim-edith
 #PBS -l nodes=1
-#PGS -l gpus=2
+#PBS -l gpus=2
 #PBS -l walltime=00:05:00
 #PBS -l filesystems=home
 
-source "$HOME/.bashrc"
+source "$HOME/.bashrc" # this makes conda availbe on the execution nodes
 conda activate torch-sim-edith
 python "$HOME/torch-sim-edith/soft_sphere_fire.py"
